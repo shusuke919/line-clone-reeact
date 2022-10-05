f18ff84cab3c9f7a9581e4907289cfa9c1378572
shusuke919 <sh091965@gmail.com>
2022-10-05 10:05:37 +0900

    221005

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import './App.css';
 import SignIn from './components/SignIn';
 
-import { auth } from './firebase';
-import { useAuthState } from 'react-firebase-hooks/auth';
+
 
 function App() {
 
-  const [user] = useAuthState(auth)
-  console.log(user)
+
+
   return (
     <div >
-    <SignIn />
+      {/* {user ? <Line /> : <SignIn />} */}
+      <SignIn />
     </div>
   );
 }
--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,31 +1,34 @@
 import React from 'react'
-
-import { auth } from '../firebase';
+import { auth, db } from '../firebase';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import GoogleButton from 'react-google-button';
 import { GoogleAuthProvider, signInWithRedirect } from 'firebase/auth';
-import { doc, setDoc } from "firebase/firestore";
+import { doc, setDoc } from "firebase/firestore"
+import SignOut from './SignOut'
 
 
-
+///ユーザー登録　Google認証
 const googleSignIn = () => {
-   const provider =new GoogleAuthProvider()
-   signInWithRedirect(auth, provider)
-
-   
-   
-}
-
+   const provider =new GoogleAuthProvider();
+   signInWithRedirect(auth, provider);
+};
 
 const SignIn = () => {
-  const user =useAuthState(auth)
-  console.log(user)
+  //useAuthState認証情報取得
+  const [user] = useAuthState(auth);
+console.log(user);
 
-  
+  //fire cloudへ保存
+  setDoc(doc(db, "users", user.uid), {
+    uid: user.uid,
+    displayName: user.displayName,
+  });
+ 
   return (
     <div>
+
       <GoogleButton onClick={googleSignIn}/>
-     
+     <SignOut/>
     </div>
   )
 }
