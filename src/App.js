222f5c2a9505753d4915b7d20f073b7c8c93c1c0
shusuke919 <sh091965@gmail.com>
2022-10-11 11:46:05 +0900

    ユーザー一覧作成中

--- src/App.js
+++ src/App.js
@@ -1,19 +1,39 @@
 import './App.css';
-import SignIn from './components/SignIn';
-import Line from './components/Line';
+import Line from './pages/Line';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { auth } from './firebase';
+import { BrowserRouter, Routes, Route, Navigate} from "react-router-dom";
+import SignIn from './pages/SignIn';
+import { useContext } from 'react';
+import { AuthContext } from './context/AuthContext';
 
 
 
 function App() {
+
+  const {currentUser} = useContext(AuthContext);
 const [user] =useAuthState(auth);
 
+// console.log(currentUser); ログイン状態か確認する
 
+//保護されたログアウトルートを作る
+// ? : は使わない
+const ProtectedRoute = ({children}) => {
+  if(!currentUser){
+    return <Navigate to="/sigin" />
+  }
+  //戻らなければ画面に何も表示されない
+  return children;
+}
   return (
-    <div >
-      {user ? <Line /> : <SignIn />}
-    </div>
+   <BrowserRouter>
+   <Routes>
+    <Route path='/'>
+      <Route index element={<ProtectedRoute><Line /></ProtectedRoute>} />
+      <Route path="sigin" element={<SignIn />} />
+    </Route>
+   </Routes>
+   </BrowserRouter>
   );
 }
 
